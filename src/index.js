4162a4da5488519b83b6ed6ba97ddcae021252f1
Dr. Derek Austin [aka dj D-REK from Richmond, VA] <53202242+djD-REK@users.noreply.github.com>
2019-08-11 14:45:37 -0400

    Added comparison to counter example using React Hooks
    Counter works fine with setState and counter variable
    
    Sourced from https://medium.com/javascript-in-plain-english/state-management-with-react-hooks-no-redux-or-context-api-8b3035ceecf8

--- src/index.js
+++ src/index.js
@@ -4,19 +4,32 @@ import ReactDOM from "react-dom";
 import "./styles.css";
 
 function App() {
-  const [state, setState] = useState(false);
+  const [stateTF, setStateTF] = useState(false);
   const toggleTrueFalse = () => {
     // setState(!state);
   };
   const getTrueFalseAsString = () => {
-    if (state) return "true";
+    if (stateTF) return "true";
     else return "false";
   };
 
+  const [state, setState] = useState({ counter: 0 });
+  const add1ToCounter = () => {
+    const newCounterValue = state.counter + 1;
+    setState({ counter: newCounterValue });
+  };
+
   return (
-    <div className="App">
-      <p>You clicked {getTrueFalseAsString()} times</p>
-      <button onClick={toggleTrueFalse()}>Click me</button>
+    <div>
+      <div className="App">
+        <p>You clicked {getTrueFalseAsString} times</p>
+        <button onClick={toggleTrueFalse}>Click me</button>
+      </div>
+
+      <div>
+        <p>You clicked {state.counter} times</p>
+        <button onClick={add1ToCounter}>Click me</button>
+      </div>
     </div>
   );
 }
