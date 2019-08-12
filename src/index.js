92ccdc946b6fe05edabad89c8c3683111e22ec90
Dr. Derek Austin [aka dj D-REK from Richmond, VA] <53202242+djD-REK@users.noreply.github.com>
2019-08-12 12:22:29 -0400

    Refactor state setting code, added boolean .toString function
    Also added emoji spans

--- src/index.js
+++ src/index.js
@@ -4,36 +4,40 @@ import ReactDOM from "react-dom";
 import "./styles.css";
 
 function App() {
-  const [state, setState] = useState({ isTrue: false, counter: 0 });
+  const [toggleState, setToggleState] = useState({ isTrue: false });
+  const [counterState, setCounterState] = useState({ counter: 0 });
+
   const toggleTrueFalse = () => {
-    setState({ isTrue: !state.isTrue, counter: state.counter });
-    // A pure toggle wouldn't need to reference counter, just isTrue,
-    // but in this implementation we are using only one state for both.
-    // In this code example, counter is required to make both work.
+    setToggleState({ isTrue: !toggleState.isTrue });
   };
   const getTrueFalseAsString = () => {
-    if (state.isTrue) return "true";
-    else return "false";
-    // for whatever reason, returning state.isTrue.toString() crashes
-    // the counter button but not the toggle button
+    return toggleState.isTrue.toString();
   };
   const incrementCounter = () => {
-    setState({ counter: state.counter + 1 });
+    setCounterState({ counter: counterState.counter + 1 });
   };
 
   return (
     <div className="App">
       <div className="container">
-        <h1>⭐️❤️☕🍍☄⛱⛷☀️☘️</h1>
+        <h1>
+          <span role="img" aria-label="Decorative Emojis">
+            ⭐️❤️☕🍍☄⛱⛷☀️☘️
+          </span>
+        </h1>
         <h2>Your toggle is {getTrueFalseAsString()} 🥳</h2>
         <button onClick={toggleTrueFalse}>
           <h3>Toggle me</h3>
         </button>
-        <h2>You clicked below {state.counter} times 🥳</h2>
+        <h2>You clicked below {counterState.counter} times 🥳</h2>
         <button onClick={incrementCounter}>
           <h3>Click me</h3>
         </button>
-        <h1>⭐️❤️☕🍍☄⛱⛷☀️☘️</h1>
+        <h1>
+          <span role="img" aria-label="Decorative Emojis">
+            ⭐️❤️☕🍍☄⛱⛷☀️☘️
+          </span>
+        </h1>
       </div>
     </div>
   );
