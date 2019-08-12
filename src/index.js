536c8c9cbbddcbfe4e360295c3ac0aaaacea84fe
Dr. Derek Austin [aka dj D-REK from Richmond, VA] <53202242+djD-REK@users.noreply.github.com>
2019-08-12 00:02:28 -0400

    Flexbox fix -- added container to correct centering of CSS Flexbox items

--- src/index.js
+++ src/index.js
@@ -22,16 +22,18 @@ function App() {
 
   return (
     <div className="App">
-      <h1>⭐️❤️☕🍍☄⛱⛷☀️☘️</h1>
-      <h2>Your toggle is {getTrueFalseAsString()} 🥳</h2>
-      <button onClick={toggleTrueFalse}>
-        <h3>Toggle me</h3>
-      </button>
-      <h2>You clicked below {state.counter} times 🥳</h2>
-      <button onClick={add1ToCounter}>
-        <h3>Click me</h3>
-      </button>
-      <h1>⭐️❤️☕🍍☄⛱⛷☀️☘️</h1>
+      <div className="container">
+        <h1>⭐️❤️☕🍍☄⛱⛷☀️☘️</h1>
+        <h2>Your toggle is {getTrueFalseAsString()} 🥳</h2>
+        <button onClick={toggleTrueFalse}>
+          <h3>Toggle me</h3>
+        </button>
+        <h2>You clicked below {state.counter} times 🥳</h2>
+        <button onClick={add1ToCounter}>
+          <h3>Click me</h3>
+        </button>
+        <h1>⭐️❤️☕🍍☄⛱⛷☀️☘️</h1>
+      </div>
     </div>
   );
 }
